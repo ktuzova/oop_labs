fd32609e6dcba0cd3cf3db895f59aa7f8777d924
ktuzova <Ksenia.Tuzova12@yandex.ru>
2024-03-03 19:18:08 +0200

    111

--- oop_lab1/test.cpp
+++ oop_lab1/test.cpp
@@ -32,6 +32,69 @@ TEST(CheckTest, Воскресенье_neg666) {
     EXPECT_TRUE(checkPhobia("Воскресенье", -666));
 }
 
+TEST(CheckTest, Суббота_57) {
+    EXPECT_FALSE(checkPhobia("Суббота", 57));
+}
+
+TEST(CheckTest, Воскресенье_555) {
+    EXPECT_FALSE(checkPhobia("Воскресенье", 555));
+}
+
+TEST(CheckTest, Вторник_90) {
+    EXPECT_FALSE(checkPhobia("Вторник", 90));
+}
+
+TEST(CheckTest, Понедельник_10) {
+    EXPECT_FALSE(checkPhobia("Понедельник", 10));
+}
+
+TEST(CheckTest, Среда_35) {
+    EXPECT_FALSE(checkPhobia("Среда", 35));
+}
+
+TEST(CheckTest, Четверг_1) {
+    EXPECT_FALSE(checkPhobia("Четверг", 1));
+}
+
+TEST(CheckTest, Пятница_21) {
+    EXPECT_FALSE(checkPhobia("Пятница", 21));
+}
+
+TEST(CheckTest, Суббота_55) {
+    EXPECT_FALSE(checkPhobia("Суббота", 55));
+}
+
+TEST(CheckTest, Воскресенье_667) {
+    EXPECT_FALSE(checkPhobia("Воскресенье", 667));
+}
+
+TEST(CheckTest, Воскресенье_neg665) {
+    EXPECT_FALSE(checkPhobia("Воскресенье", -665));
+}
+
+TEST(CheckTest, Понедельник_11) {
+    EXPECT_FALSE(checkPhobia("Понедельник", 11));
+}
+
+TEST(CheckTest, Среда_33) {
+    EXPECT_FALSE(checkPhobia("Среда", 33));
+}
+
+TEST(CheckTest, Четверг_5) {
+    EXPECT_FALSE(checkPhobia("Четверг", 5));
+}
+
+TEST(CheckTest, Пятница_19) {
+    EXPECT_FALSE(checkPhobia("Пятница", 19));
+}
+
+TEST(CheckTest, Суббота_54) {
+    EXPECT_FALSE(checkPhobia("Суббота", 54));
+}
+
+
+
+
 int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
